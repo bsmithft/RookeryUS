0d8e757cfd83dfd3ded6b0f444976fa46c7dfc12
bsmithft <70830813+bsmithft@users.noreply.github.com>
2023-01-09 08:05:00 -0600

    sf

--- test.js
+++ test.js
@@ -1,11 +1,27 @@
-// grab the element
-var el = document.getElementById('idOfElement');
-
-// listen for the long-press event
-el.addEventListener('long-press', function(e) {
-
-  // stop the event from bubbling up
-  e.preventDefault()
-  navigator.vibrate(50);
-  console.log(e.target);
+const shopping = document.getElementById('shopping');
+shopping.addEventListener('mousedown' , function() {
+    pressTimer = window.setTimeout(longpressed,1000);
 });
+shopping.addEventListener('mouseup' , function(e) {
+    clearTimeout(pressTimer);
+});
+
+shopping.addEventListener('click' , function(e) {
+    console.log('click');
+});
+
+function longpressed() {
+    console.log('longpress');
+    window.navigator.vibrate(50);
+    window.addEventListener(
+        'click',
+        captureClick,
+        true // <-- This registers this listener for the capture
+             //     phase instead of the bubbling phase!
+    );
+}
+
+function captureClick(e) {
+    e.stopPropagation(); // Stop the click from being propagated.
+    window.removeEventListener('click', captureClick, true); // cleanup
+}
