46483ed08b5c73d1241b1fb654aeb9c907a791f0
bsmithft <70830813+bsmithft@users.noreply.github.com>
2023-01-10 16:39:49 -0600

    Update test.js

--- test.js
+++ test.js
@@ -9,40 +9,34 @@ var mc = new Hammer(myElement);
 mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
 // listen to events...
-mc.on("panleft panright tap press", function(ev) {
+mc.on("panleft panright panup pandown tap press", function(ev) {
   myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.")});
 
 mc.on("panleft", function(ev) {
-  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
     window.navigator.vibrate(50);
 });
 mc.on("panright", function(ev) {
-  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
 
 });
 mc.on("panup", function(ev) {
-  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
 
 });
 mc.on("pandown", function(ev) {
-  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
 
 });
 mc.on("tap", function(ev) {
-  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
 });
 mc.on("press", function(ev) {
-  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
 });
