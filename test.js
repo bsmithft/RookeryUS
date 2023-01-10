9d8af30cb94df2b273d644e2caeba09799e0adf5
bsmithft <70830813+bsmithft@users.noreply.github.com>
2023-01-10 15:35:24 -0600

    sfs

--- test.js
+++ test.js
@@ -13,27 +13,27 @@ mc.on("panleft", function(ev) {
     //myElement.textContent = ;
     console.log(ev.type +" gesture detected.");
     window.navigator.vibrate(50);
-    window.location.href = "http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States"
+    window.location.href = 'http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States'
 });
 mc.on("panright", function(ev) {
   //myElement.textContent = ;
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
-  window.location.href = "http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States"
+  window.location.href = 'http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States'
 
 });
 mc.on("panup", function(ev) {
   //myElement.textContent = ;
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
-  window.location.href = "http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States"
+  window.location.href = 'http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States'
 
 });
 mc.on("pandown", function(ev) {
   //myElement.textContent = ;
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
-  window.location.href = "http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States"
+  window.location.href = 'http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States'
 
 });
 mc.on("tap", function(ev) {
