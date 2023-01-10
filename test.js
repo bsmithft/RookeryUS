1148f3420834ce37c1b1e07e0605e5f0db56a321
bsmithft <70830813+bsmithft@users.noreply.github.com>
2023-01-10 16:38:47 -0600

    Update test.js

--- test.js
+++ test.js
@@ -9,6 +9,10 @@ var mc = new Hammer(myElement);
 mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
 // listen to events...
+mc.on("panleft panright tap press", function(ev) {
+  myElement.textContent = ev.type +" gesture detected.";
+  console.log(ev.type +" gesture detected.")});
+
 mc.on("panleft", function(ev) {
   myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
