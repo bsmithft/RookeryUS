786bb9caaa1b7ca12c59a0d7e973a7ea1d3d7e80
bsmithft <70830813+bsmithft@users.noreply.github.com>
2023-01-10 08:18:38 -0600

    Update test.js

--- test.js
+++ test.js
@@ -9,10 +9,36 @@ var mc = new Hammer(myElement);
 mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
 // listen to events...
-mc.on("panleft panright panup pandown tap press", function(ev) {
+mc.on("panleft", function(ev) {
     //myElement.textContent = ;
     console.log(ev.type +" gesture detected.");
     window.navigator.vibrate(50);
+    window.location.href = "https://www.google.com"
+});
+mc.on("panright", function(ev) {
+  //myElement.textContent = ;
+  console.log(ev.type +" gesture detected.");
+  window.navigator.vibrate(50);
+});
+mc.on("panup", function(ev) {
+  //myElement.textContent = ;
+  console.log(ev.type +" gesture detected.");
+  window.navigator.vibrate(50);
+});
+mc.on("pandown", function(ev) {
+  //myElement.textContent = ;
+  console.log(ev.type +" gesture detected.");
+  window.navigator.vibrate(50);
+});
+mc.on("tap", function(ev) {
+  //myElement.textContent = ;
+  console.log(ev.type +" gesture detected.");
+  window.navigator.vibrate(50);
+});
+mc.on("press", function(ev) {
+  //myElement.textContent = ;
+  console.log(ev.type +" gesture detected.");
+  window.navigator.vibrate(50);
 });
 hammerTime.on('panleft', () => {
     window.location.href = "https://www.google.com"
