5697da0c56381277d007779b0828c11c9e63555f
bsmithft <70830813+bsmithft@users.noreply.github.com>
2023-01-10 16:07:40 -0600

    gyfdcytc

--- test.js
+++ test.js
@@ -10,39 +10,35 @@ mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
 // listen to events...
 mc.on("panleft", function(ev) {
-    //myElement.textContent = ;
-    console.log(ev.type +" gesture detected.");
+  myElement.textContent = ev.type +" gesture detected.";
+  console.log(ev.type +" gesture detected.");
     window.navigator.vibrate(50);
-    window.location = 'http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States'
 });
 mc.on("panright", function(ev) {
-  //myElement.textContent = ;
+  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
-  window.location = 'http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States'
 
 });
 mc.on("panup", function(ev) {
-  //myElement.textContent = ;
+  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
-  window.location.replace = 'http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States'
 
 });
 mc.on("pandown", function(ev) {
-  //myElement.textContent = ;
+  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
-  window.location.replace = 'http://maps.apple.com/?lsp=9902&auid=4158007438164491913&sll=38.897517,-77.036542&q=The%20White%20House&hnear=1600%20Pennsylvania%20Ave%20NW,%20Washington,%20DC%2020500-0003,%20United%20States'
 
 });
 mc.on("tap", function(ev) {
-  //myElement.textContent = ;
+  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
 });
 mc.on("press", function(ev) {
-  //myElement.textContent = ;
+  myElement.textContent = ev.type +" gesture detected.";
   console.log(ev.type +" gesture detected.");
   window.navigator.vibrate(50);
 });
