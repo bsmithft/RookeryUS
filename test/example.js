70e18dd754a17b1cbf22f88df8bcdd4800be7000
bsmithft <70830813+bsmithft@users.noreply.github.com>
2023-02-02 09:44:38 -0600

    kjhvb

--- test/example.js
+++ test/example.js
@@ -1,9 +1,12 @@
+var sw42base = document.getElementById('sw42base');
+var mc = new Hammer(sw42base);
 var sw42name = document.getElementById('sw42name');
 var mc = new Hammer(sw42name);
 var sw42menu = document.getElementById('sw42menu');
 var mc = new Hammer(sw42menu);
 var sw42hours = document.getElementById('sw42hours');
 var mc = new Hammer(sw42hours);
+
 mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
 mc.on("tap", function(ev) {
