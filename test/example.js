fc2dacb528a78ea69a6b2f964fd1e7aa0d8c53e9
bsmithft <70830813+bsmithft@users.noreply.github.com>
2023-02-02 08:53:59 -0600

    test 0853020223

--- test/example.js
+++ test/example.js
@@ -1,8 +1,9 @@
-
-var myElement = document.getElementById('myElement');
-var mc = new Hammer(myElement);
+var sw42name = document.getElementById('sw42name');
+var mc = new Hammer(sw42name);
 var sw42menu = document.getElementById('sw42menu');
 var mc = new Hammer(sw42menu);
+var sw42hours = document.getElementById('sw42hours');
+var mc = new Hammer(sw42hours);
 mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
 mc.on("tap", function(ev) {
